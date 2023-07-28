ded9ae6fb67e4804bc4699b4766a4a430431adfc
Kors12614 <selim.benkemla@gmail.com>
2023-07-28 11:13:35 +0100

    ajfzef

---
+++ main.js
@@ -0,0 +1,133 @@
+const quizData = [
+    {
+      question: "Which language runs in a web browser?",
+      a: "Java",
+      b: "C",
+      c: "Python",
+      d: "JavaScript",
+      correct: "d",
+    },
+    {
+      question: "What does CSS stand for?",
+      a: "Central Style Sheets",
+      b: "Cascading Style Sheets",
+      c: "Cascading Simple Sheets",
+      d: "Cars SUVs Sailboats",
+      correct: "b",
+    },
+    {
+      question: "What does HTML stand for?",
+      a: "Hypertext Markup Language",
+      b: "Hypertext Markdown Language",
+      c: "Hyperloop Machine Language",
+      d: "Helicopters Terminals Motorboats Lamborginis",
+      correct: "a",
+    },
+    {
+      question: "What year was JavaScript launched?",
+      a: "1996",
+      b: "1995",
+      c: "1994",
+      d: "none of the above",
+      correct: "b",
+    },
+    {
+        question: "What does HTTP stand for ?",
+        a: "Hypertext Transfer Protocol",
+        b: "Hypertext Tunisian protocol",
+        c: "1Hypertext Terminate Protocol",
+        d: "none of the above",
+        correct: "a",
+      },
+      {
+        question: "What does WWW stand for?",
+        a: "World Wide WEB",
+        b: "Who When Why",
+        c: "Willy Willy Wonka",
+        d: "none of the above",
+        correct: "a",
+      },
+      {
+        question: "What year was Choufli hal launched?",
+        a: "October 2005",
+        b: "September 2009",
+        c: "September 2006",
+        d: "none of the above",
+        correct: "a",
+      },
+      {
+        question: "Who is rabi booden?",
+        a: "a meme",
+        b: "a man of culture",
+        c: "nobody",
+        d: "the best tv presentator",
+        correct: "b",
+      },
+      {
+        question: "Who is the fastest human on earth?",
+        a: "Usain Bolt",
+        b: "Kylian Mbappé",
+        c: "Pessi",
+        d: "Penaldo",
+        correct: "a",
+      },
+      {
+        question: "What is the best cb of all time?",
+        a: "Pepe",
+        b: "ramos",
+        c: "Maldini",
+        d: "Vidic",
+        correct: "c",
+      },
+  ];
+  
+  const quiz = document.getElementById("quiz");
+  const answerElements = document.querySelectorAll(".answer");
+  const questionElement = document.getElementById("question");
+  const a_text = document.getElementById("a_text");
+  const b_text = document.getElementById("b_text");
+  const c_text = document.getElementById("c_text");
+  const d_text = document.getElementById("d_text");
+  const submitButton = document.getElementById("submit");
+  
+  let currentQuiz = 0;
+  let score = 0;
+  
+  const deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+  };
+  
+  const getSelected = () => {
+    let answer;
+    answerElements.forEach((answerElement) => {
+      if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+  };
+  
+  const loadQuiz = () => {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+  };
+  
+  loadQuiz();
+  
+  submitButton.addEventListener("click", () => {
+    const answer = getSelected();
+    if (answer) {
+      if (answer === quizData[currentQuiz].correct) score++;
+      currentQuiz++;
+      if (currentQuiz < quizData.length) loadQuiz();
+      else {
+        quiz.innerHTML = `
+              <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+              <button onclick="history.go(0)">Play Again</button>
+          ` 
+      }
+    }
+  });
